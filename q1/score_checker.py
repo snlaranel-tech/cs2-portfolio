0034031ed94c044e57fd2b0c6d4544260c8ca9de
snlaranel-tech <snlaranel@brc.pshs.edu.ph>
2026-09-06 19:23:44 +0800

    Refine comments in score_checker.py
    Updated comments for clarity and improved grammar.

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,13 +1,13 @@
-# Ask the user to input their numerical grade as an integer
+# Asks the user to input their numerical grade as an integer.
 score = int(input("Enter your score: "))
 
-#Validate if the entered score is within the allowed range
-# Ask for a new number if the entered value is outside the allowed range
+#Validates if the entered numerical score is within the allowed range of 0 to 100.
+#Asks for a new number if the entered value is outside the allowed range using a loop.
 while score < 0 or score > 100:
     print("Invalid score")
     score = int(input("Please enter a valid number: ")) 
 
-#Determine the values aappropriate performance classification
+#Determines the values aappropriate performance classification to be printed.
 if 90 <= score <= 100:
   print("Outstanding")
 elif 80 <= score < 90:
