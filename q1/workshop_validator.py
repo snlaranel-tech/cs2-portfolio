edf0372f7156d3b63e34dbceefaa373ccf4d707f
snlaranel-tech <snlaranel@brc.pshs.edu.ph>
2026-09-09 21:18:46 +0800

    Update workshop_validator.py

--- q1/workshop_validator.py
+++ q1/workshop_validator.py
@@ -14,28 +14,30 @@ def print_error(message):
   return
 
 # Name validation
-name = input("Enter student name: ").strip()
+name = (input("Enter student name: ").strip())
 if not name:
   print_error ("Student name is required.")
 
 # Age validation
-age_input = input("Enter age: ").strip()
+age_input = input("Enter student age: ").strip()
 age = int(age_input)
 if not (11 <= age <= 18):
   print_error ("Age must be from 11 to 18")
 
 # Grade level (gl) validation
-grade_level = int(input("Enter grade level: ").strip())
-valid_gl = (7, 8, 9, 10, 11, 12)
-if not grade_level >= 7 and grade_level <= 12:
-  print_error ("Invalid Grade Level")
+try:
+  grade_level = int(input("Enter student grade level: ").strip())
+  if not grade_level >= 7 and grade_level <= 12:
+    print_error ("Invalid Grade Level")
+except ValueError:
+  print_error("Grade level must be a valid number.")
 
 # Email validation
-email = input("Enter email: ")
+email = input("Enter student email: ")
 if "@" in email and "." in email:
     pass
 else:
-    print_error ("Invalid email")
+  print_error ("Invalid email")
 
 # Registration code validation
 # len(regis_code) counts how many characters are entered in regis_code
@@ -44,7 +46,6 @@ regis_code = input("Enter workshop registration code: ").strip()
 if len(regis_code) != 6:
   print_error ("The registration code must contain exactly 6 characters.")
 
-# Final evaluated output display, registration accepted or not
 if is_valid:
   print ("\n------------------------------")
   print ("REGISTRATION ACCEPTED!!")
